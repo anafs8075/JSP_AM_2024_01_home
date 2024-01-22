d3825ca9fbe9c9c288e2e31dd2bc4a4419c67b20
anafs8075 <anafs8075@gmail.com>
2024-01-23 02:39:40 +0900

    서블릿이 JSP 파일에게 요청 정보를 넘겨줘서 협업이 가능하도록

--- src/main/java/com/KoreaIT/java/Jsp_AM/HomePrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/HomePrintDanServlet.java
@@ -7,14 +7,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home/main2")
-public class HomeMainServlet2 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+@WebServlet("/home/printDan")
+public class HomePrintDanServlet extends HttpServlet {
        
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		response.getWriter().append("Hello World!!!!!!!!!!!!!!!!!!!!!!!").append(request.getContextPath());
+		request.getRequestDispatcher("/jps/home/printDan.jsp").forward(request, response);
 	}
 
 
--- src/main/java/com/KoreaIT/java/Jsp_AM/prindDanServelet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/prindDanServelet.java
@@ -10,19 +10,18 @@ import java.io.IOException;
 @WebServlet("/printDan")
 public class prindDanServelet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		response.setContentType("text/html;charset=UTF-8");
-		
+
 		String inputedDan = request.getParameter("dan");
 		String inputedLimit = request.getParameter("limit");
 		String inputedColor = request.getParameter("color");
-		
+
 		System.out.println(inputedDan);
-		
+
 		if (inputedDan == null) {
 			inputedDan = "1";
 		}
@@ -32,17 +31,17 @@ public class prindDanServelet extends HttpServlet {
 		if (inputedColor == null) {
 			inputedColor = "black";
 		}
-		
+
 		int dan = Integer.parseInt(inputedDan);
 		int limit = Integer.parseInt(inputedLimit);
-		
-		response.getWriter().append(String.format("<div style=\"color:%s\";>==%d단==</div>",inputedColor, dan));
-		
-		for(int i = 1; i<=limit; i++) {
-			
-			response.getWriter().append(String.format("<div style=\"color:%s\";>%d * %d = %d</div>",inputedColor, dan, i, dan * i));
+
+		response.getWriter().append(String.format("<div style=\"color:%s\";>==%d단==</div>", inputedColor, dan));
+
+		for (int i = 1; i <= limit; i++) {
+
+			response.getWriter().append(
+					String.format("<div style=\"color:%s\";>%d * %d = %d</div>", inputedColor, dan, i, dan * i));
 		}
 	}
-
 
 }
